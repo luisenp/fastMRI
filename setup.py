c03d555fb5d735f3f5032ac3760d5737416de3c0
Luis Pineda <luisen.p@gmail.com>
2020-09-24 15:39:03 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as f:
+    reqs = []
+    for line in f:
+        line = line.strip()
+        reqs.append(line.split("==")[0])
+
+packages = setuptools.find_packages()
+setuptools.setup(
+    name="fastmri",
+    version="0.1.0",
+    author="Facebook AI Research",
+    description="fastMRI dataset loaders and baselines.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/facebookresearch/fastMRI/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence :: Medical Imaging",
+    ],
+    python_requires=">=3.8",
+    install_requires=reqs,
+)
